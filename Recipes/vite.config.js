bbe258931f294b89ace49923c8e53178de4a32a0
Witley Seong <witleyseong@hotmail.com>
2025-10-16 10:34:51 +0100

    vite deploy

--- Recipes/vite.config.js
+++ Recipes/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// 👇 Isso é o passo 1 — adiciona o caminho base do projeto
 export default defineConfig({
   plugins: [react()],
-  base: '/hora-do-rango/', // 👈 muito importante
+  base: '/hora-do-rango/', 
 })
