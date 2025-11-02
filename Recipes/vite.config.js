bf3edc7ed1721e59344e63dd63e7a53bc1262b9e
Witley Seong <witleyseong@hotmail.com>
2025-11-02 14:20:21 +0000

    build update

--- Recipes/vite.config.js
+++ Recipes/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 Isso é o passo 1 — adiciona o caminho base do projeto
+
 export default defineConfig({
   plugins: [react()],
   base: '/hora-do-rango/', 
